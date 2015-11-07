9eefbb0fcbc6eb83557a969b55ef96f15e7dcc43
Jaime Niswonger <jniswonger@keyholesoftware.com>
2015-11-07 10:35:14 -0600

    completed exercise 3

--- src/main/modules/app.js
+++ src/main/modules/app.js
@@ -6,6 +6,8 @@ require('./project/project');
 
 module.exports = angular.module('primer', ['primer.project'])
 
+    .directive('header', require('./directive'))
+
     .controller('AppCtrl', ['$scope', 'ProjectService',
         function ($scope, ProjectService) {
             ProjectService.projects().then(function (projects) {
---
+++ src/main/modules/directive.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = [
+    function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'views/header.html'
+        };
+    }
+];
